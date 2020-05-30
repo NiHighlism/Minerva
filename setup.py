94a29656a17b4357fcc3ab01c2a55f5980571a68
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-05-30 23:02:06 +0530

    Fix PEP8 issues

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
-from setuptools import setup, find_packages
-
+"""Setup file to distribute Minerva as a package."""
+from setuptools import find_packages, setup
 
 with open('README.md') as f:
     readme = f.read()
@@ -22,5 +18,3 @@ setup(
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
-
