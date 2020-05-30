d25aab7a1fb965d9490a3ffdf5fcd993d92e1104
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-05-31 01:33:29 +0530

    Update sanity.sh to exclude env from dodgy

--- sanity.sh
+++ sanity.sh
@@ -1,4 +1,4 @@
-dodgy --ignore paths venv, env, migrations, scripts
-isort -rc --atomic ./
+dodgy --ignore paths venv env migrations scripts
+isort -rc --atomic ./ --skip env
 pydocstyle --config=./.pydocstylerc
 pycodestyle ./ --config=./.pycodestylerc
